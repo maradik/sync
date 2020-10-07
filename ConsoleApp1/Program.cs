6d5a0f57ce24930188e2f864b5d6567091b385c4
Anton Yuzhanin <a.yuzhanin@skbkontur.ru>
2020-10-07 11:15:17 +0500

    ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var semaphore = new Semaphore(1, 1, "MySemaphore");
+            semaphore.WaitOne();
+
+            Console.WriteLine("Hello World!");
+
+            semaphore.Release();
+        }
+    }
+}
